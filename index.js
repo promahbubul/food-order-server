8880e589a7b5f114a603d6a1acd061b3e8b13d17
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2023-10-22 12:22:33 +0600

    add menu item

--- index.js
+++ index.js
@@ -1,16 +1,73 @@
-import express from "express";
-import cors from 'cors'
-const port = process.env.PORT || 5000
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5001;
+const app = express();
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Server in running 😊')
-})
+const data = {
+  name: "Maubu",
+  age: 18,
+};
+
+app.get("/", (req, res) => {
+  res.send(data);
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xmhqmx1.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const foodMenu = client.db("foodOrder").collection("foodMenu");
+
+    app.get("/menu", async (req, res) => {
+      const cursor = foodMenu.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodMenu.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/menuitem", async (req, res) => {
+      const newMenuItem = req.body;
+      console.log(newMenuItem);
+      const result = await foodMenu.insertOne(newMenuItem);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`Server is runing on http://localhost:${port}`);
-})
+  console.log(`Server is runing on http://localhost:${port}`);
+});
